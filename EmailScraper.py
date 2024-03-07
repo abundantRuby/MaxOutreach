a3d2b70cf0ebf88808df51fbf5ea1eb4f7e9ae70
Dylan Hoag <95001739+abundantRuby@users.noreply.github.com>
2024-03-07 12:01:27 -0700

    Update EmailScraper.py

--- EmailScraper.py
+++ EmailScraper.py
@@ -13,7 +13,7 @@ from multiprocessing import Pool
 # --------------------------------------- VARIABLES TO EXECUTE ---------------------------------------------------#
 # --------------------------------------------------------------------------------------------------------------- #
         
-yelp_urls = ["https://www.yelp.com/search?find_desc=painters&find_loc=Pheonix%2C+AZ"]
+yelp_urls = ["https://www.yelp.com/search?find_desc=painters&find_loc=Tucson%2C+AZ"]
 
 search_term = "painters"
 
